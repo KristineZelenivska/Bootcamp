fadd82b42e2e6133d3056ac32a798638174de895
Student <Student@LVAGPLTP0847_BC.dir.svc.accenture.com>
2019-01-31 14:00:32 +0200

    commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+function something() {
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function(){
+            resolve("done");
+        },3000);
+        }
+    ) 
+         promise.then(function(result){
+      console.log(result);
+         }
+  )
+}
+something();
